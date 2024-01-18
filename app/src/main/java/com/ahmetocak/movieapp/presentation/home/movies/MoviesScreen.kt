30aaea93eef8cc85fd5d13a9d8abee6e83b91555
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-01-18 15:11:37 +0300

    movie item moved to ui/components package

--- app/src/main/java/com/ahmetocak/movieapp/presentation/home/movies/MoviesScreen.kt
+++ app/src/main/java/com/ahmetocak/movieapp/presentation/home/movies/MoviesScreen.kt
@@ -1,38 +1,29 @@
 package com.ahmetocak.movieapp.presentation.home.movies
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.ahmetocak.movieapp.R
-import com.ahmetocak.movieapp.presentation.ui.components.AnimatedAsyncImage
+import com.ahmetocak.movieapp.presentation.ui.components.MovieItem
 import com.ahmetocak.movieapp.presentation.ui.components.MovieScaffold
 import com.ahmetocak.movieapp.presentation.ui.theme.MovieAppTheme
 import com.ahmetocak.movieapp.utils.ScreenPreview
@@ -127,56 +118,6 @@ private fun ContentTitleSection(text: String, onSeeAllClicked: () -> Unit) {
         )
         Button(onClick = onSeeAllClicked) {
             Text(text = stringResource(id = R.string.see_all_text))
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun MovieItem(
-    modifier: Modifier = Modifier,
-    id: Int,
-    name: String,
-    categories: List<String>,
-    imageUrl: String,
-    voteAverage: Double,
-    voteCount: Int,
-    onClick: (Int) -> Unit
-) {
-    ElevatedCard(
-        modifier = modifier,
-        onClick = { onClick(id) }) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            AnimatedAsyncImage(modifier = Modifier.fillMaxSize(), imageUrl = imageUrl)
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(dimensionResource(id = R.dimen.two_level_padding)),
-                verticalArrangement = Arrangement.Bottom
-            ) {
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                    color = Color.White
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = buildString { append(categories.joinToString(", ")) },
-                    color = Color.White
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.one_level_padding)),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Star,
-                        contentDescription = null,
-                        tint = Color.Yellow
-                    )
-                    Text(text = "$voteAverage ($voteCount)", color = Color.White)
-                }
-            }
         }
     }
 }
---
+++ app/src/main/java/com/ahmetocak/movieapp/presentation/ui/components/MovieItem.kt
@@ -0,0 +1,75 @@
+package com.ahmetocak.movieapp.presentation.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.ahmetocak.movieapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieItem(
+    modifier: Modifier = Modifier,
+    id: Int,
+    name: String,
+    categories: List<String>,
+    imageUrl: String,
+    voteAverage: Double,
+    voteCount: Int,
+    onClick: (Int) -> Unit
+) {
+    ElevatedCard(
+        modifier = modifier,
+        onClick = { onClick(id) }) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            AnimatedAsyncImage(modifier = Modifier.fillMaxSize(), imageUrl = imageUrl)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(dimensionResource(id = R.dimen.two_level_padding)),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = buildString { append(categories.joinToString(", ")) },
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.one_level_padding)),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = null,
+                        tint = Color.Yellow
+                    )
+                    Text(text = "$voteAverage ($voteCount)", color = Color.White)
+                }
+            }
+        }
+    }
+}
