6f2604f6efcc17eeff15542a82430a702e81b5a1
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-01-24 14:47:51 +0300

    hilt initialized

---
+++ app/src/main/java/com/ahmetocak/movieapp/MovieApplication.kt
@@ -0,0 +1,7 @@
+package com.ahmetocak.movieapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieApplication : Application()
--- app/src/main/java/com/ahmetocak/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ahmetocak/movieapp/presentation/MainActivity.kt
@@ -1,16 +1,17 @@
-package com.ahmetocak.movieapp
+package com.ahmetocak.movieapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.ahmetocak.movieapp.presentation.MovieApp
 import com.ahmetocak.movieapp.presentation.navigation.MainDestinations
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieApp(startDestination = MainDestinations.HOME_ROUTE)
+            MovieApp(startDestination = MainDestinations.SIGN_UP_ROUTE)
         }
     }
 }
