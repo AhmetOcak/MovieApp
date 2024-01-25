71732f43773669e86bbd4ac1fba58f800d4beb66
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-01-25 14:23:51 +0300

    io dispatcher added to app module

--- app/src/main/java/com/ahmetocak/movieapp/di/AppModule.kt
+++ app/src/main/java/com/ahmetocak/movieapp/di/AppModule.kt
@@ -14,6 +14,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
 @Module
@@ -34,5 +36,11 @@ object AppModule {
                 context.preferencesDataStoreFile(DataStoreConstants.FILE_NAME)
             }
         )
+    }
+
+    @Singleton
+    @Provides
+    fun provideIoDispatcher(): CoroutineDispatcher {
+        return Dispatchers.IO
     }
 }
