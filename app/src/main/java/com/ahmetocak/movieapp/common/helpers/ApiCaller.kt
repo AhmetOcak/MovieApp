880c3d41b2523f9b81814e5df68297c8537109fa
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-01-25 17:45:56 +0300

    api caller helper class added

---
+++ app/src/main/java/com/ahmetocak/movieapp/common/helpers/ApiCaller.kt
@@ -0,0 +1,19 @@
+package com.ahmetocak.movieapp.common.helpers
+
+import android.util.Log
+import com.ahmetocak.movieapp.R
+import com.ahmetocak.movieapp.common.Response
+import java.io.IOException
+
+suspend inline fun <T> apiCall(crossinline call: suspend () -> T): Response<T> {
+    return try {
+        Response.Success(data = call())
+    } catch (e: IOException) {
+        Log.e("API CALL EXCEPTION", e.stackTraceToString())
+        Response.Error(errorMessage = UiText.StringResource(R.string.internet_error))
+    } catch (e: Exception) {
+        Log.e("API CALL EXCEPTION", e.stackTraceToString())
+        Response.Error(errorMessage = e.message?.let { message -> UiText.DynamicString(message) }
+            ?: UiText.StringResource(R.string.unknown_error))
+    }
+}
