9d65b653ed2701626330ad329f7a2c8510950faf
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-01-25 17:45:23 +0300

    full screen progress indicator component added

--- app/src/main/java/com/ahmetocak/movieapp/presentation/ui/components/CircularProgressIndicator.kt
+++ app/src/main/java/com/ahmetocak/movieapp/presentation/ui/components/CircularProgressIndicator.kt
@@ -1,8 +1,11 @@
 package com.ahmetocak.movieapp.presentation.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -12,4 +15,11 @@ fun ButtonCircularProgressIndicator() {
         modifier = Modifier.size(24.dp),
         strokeWidth = 2.dp
     )
+}
+
+@Composable
+fun FullScreenCircularProgressIndicator() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+    }
 }
