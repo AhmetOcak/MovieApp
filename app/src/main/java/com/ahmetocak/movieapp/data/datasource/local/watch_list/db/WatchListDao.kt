be588a752029dfeefcb95747e42e38140d55e9c3
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-01-30 09:52:10 +0300

    conflict strategy set replace

--- app/src/main/java/com/ahmetocak/movieapp/data/datasource/local/watch_list/db/WatchListDao.kt
+++ app/src/main/java/com/ahmetocak/movieapp/data/datasource/local/watch_list/db/WatchListDao.kt
@@ -2,13 +2,14 @@ package com.ahmetocak.movieapp.data.datasource.local.watch_list.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ahmetocak.movieapp.model.watch_list.WatchListEntity
 
 @Dao
 interface WatchListDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addMovieToWatchList(watchListEntity: WatchListEntity)
 
     @Query("SELECT * FROM WatchListEntity")
