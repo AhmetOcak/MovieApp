3dd20758a90f377c32d80d718df761e52e7a398c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2024-02-03 11:46:26 +0300

    language query added

--- app/src/main/java/com/ahmetocak/movieapp/data/datasource/remote/movie/api/MovieApi.kt
+++ app/src/main/java/com/ahmetocak/movieapp/data/datasource/remote/movie/api/MovieApi.kt
@@ -8,42 +8,49 @@ import com.ahmetocak.movieapp.utils.Network
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import java.util.Locale
 
 interface MovieApi {
 
     @GET(Network.EndPoints.NOW_PLAYING)
     suspend fun getNowPlayingMovies(
         @Query(Network.Queries.PAGE) page: Int = 1,
-        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55"
+        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55",
+        @Query(Network.Queries.LANGUAGE) language: String = Locale.getDefault().toLanguageTag()
     ): Movie
 
     @GET(Network.EndPoints.POPULAR)
     suspend fun getPopularMovies(
         @Query(Network.Queries.PAGE) page: Int = 1,
-        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55"
+        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55",
+        @Query(Network.Queries.LANGUAGE) language: String = Locale.getDefault().toLanguageTag()
     ): Movie
 
     @GET(Network.EndPoints.MOVIE_DETAILS)
     suspend fun getMovieDetails(
         @Path(Network.Paths.MOVIE_ID) movieId: Int,
-        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55"
+        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55",
+        @Query(Network.Queries.LANGUAGE) language: String = Locale.getDefault().toLanguageTag()
     ): MovieDetailDto
 
     @GET(Network.EndPoints.MOVIE_CREDITS)
     suspend fun getMovieCredits(
         @Path(Network.Paths.MOVIE_ID) movieId: Int,
-        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55"
+        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55",
+        @Query(Network.Queries.LANGUAGE) language: String = Locale.getDefault().toLanguageTag()
     ): MovieCreditDto
 
     @GET(Network.EndPoints.MOVIE_TRAILERS)
     suspend fun getMovieTrailers(
         @Path(Network.Paths.MOVIE_ID) movieId: Int,
-        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55"
+        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55",
+        @Query(Network.Queries.LANGUAGE) language: String = Locale.getDefault().toLanguageTag()
     ): MovieTrailer
 
     @GET(Network.EndPoints.SEARCH_MOVIE)
     suspend fun searchMovie(
         @Query(Network.Queries.SEARCH_QUERY) query: String,
-        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55"
+        @Query(Network.Queries.API_KEY) apiKey: String = "ce10126f10a15837182856698036de55",
+        @Query(Network.Queries.LANGUAGE) language: String = Locale.getDefault().toLanguageTag()
     ): Movie
 }
